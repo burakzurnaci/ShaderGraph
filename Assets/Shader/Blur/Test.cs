44a39c5ed5e1b2c4482c5ad85149d455e46b4ef6
Burak Zurnacı <burakzurnaci@gmail.com>
2023-03-02 17:36:57 +0300

    Blur effect shader added.

---
+++ Assets/Shader/Blur/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Material blur;
+    
+    private float _index=0;
+    void Start()
+    {
+        DOTween.To(()=> _index, x=> _index = x, 0.008f, 1).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+
+    private void Update()
+    {
+        blur.SetFloat("Blur", _index);
+    }
+}
